ca8c9d509e47b19e0470c6254d366d828d454aa0
Ivor Houlker <ivor.houlker@gmail.com>
2022-12-03 00:44:52 +0800

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,27 @@
+const { mergeConfig } = require('vite');
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.tsx'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  core: {},
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return {
+      ...config,
+      define: {
+        ...config.define,
+        global: 'window',
+      },
+      esbuild: {
+        ...config.esbuild,
+        jsxInject: `import React from 'react'`,
+      },
+    };
+  },
+  docs: {
+    docsPage: 'automatic',
+  },
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# UI Components for Presence
+
+Storybook, React, Tailwindcss.
