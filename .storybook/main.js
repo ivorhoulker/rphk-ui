89a787f72f26a4f59cf63a22367f9b1e7e5ab7c0
Ivor Houlker <ivor.houlker@gmail.com>
2022-12-03 18:48:36 +0800

    webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,9 @@
-const { mergeConfig } = require('vite');
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-essentials'],
-  features: {
-    storyStoreV7: true,
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
   },
-  // framework: {
-  //   name: '@storybook/react-vite',
-  //   options: {},
-  // },
   staticDirs: ['../public'],
-  // core: {
-  //   builder: 'webpack5',
-  // },
 };
